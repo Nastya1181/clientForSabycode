57238c4fcf061f5bb3d9534177f7474b1f0e639b
Jora947 <JoraKochkin@yandex.ru>
2022-07-21 13:16:33 +0500

    Карусель добавил, не очень хорошую над переделать и там по мелочи

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,28 +6,40 @@ import { useLogoutMutation } from "../redux/api/sabycodeApi";
 import LogOutButton from "./LogOutButton";
 import LogButton from "./LogButton";
 import CloseButton from "./CloseButton";
+import Carousel from "./Carousel";
+import Members from "./Members"
 export default function Header() {
   const userName = useSelector(selectUserName);
-  const initialState = "ПРИГЛАСИТЬ";
+  const initialState = "Пригласить";
   const [buttonText, setButtonText] = useState(initialState);
   const accessToken = useSelector(selectAccessToken);
   const changeText = (text) => {
     setButtonText(text);
     setTimeout(() => setButtonText(initialState), [1000])
   }
+  const [post] = useState([
+    {name:"Иван Иванов"},
+    {name:"Даша Даревич"},
+    {name:"Даша Даревич"},
+    {name:"Даша Даревич"},
+  ])
  
   return (
     <header className="header">
       <div className="header__logo__possion">
         <div className="header__logo"></div>
       </div>
+      <Carousel show={3}>
+        {post.map(post=>
+          <Members post= {post}/>)}
+      </Carousel>
       {userName && (
         <>
           <button
             className="invite-button"
             onClick={() => {
               navigator.clipboard.writeText(window.location);
-              changeText("СКОПИРОВАНО")
+              changeText("Скопировано")
             }}
           >
             <div className="invite-button__text">{buttonText}</div>
---
+++ src/components/Members.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Members(props) {
+    return (
+      <>
+      <div className="members__text">{props.post.name}</div>
+      </>
+    );
+  }
+
+export default Members;
