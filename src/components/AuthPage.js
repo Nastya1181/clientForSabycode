dcac59829e3e1a53f4cba7413497722027297c1f
Nastya <girnovaav@gmail.com>
2022-07-27 19:52:03 +0500

    раскомментировала GoogleAuth

--- src/components/AuthPage.js
+++ src/components/AuthPage.js
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux/es/exports";
 import {  selectUserName } from "../redux/features/authentication/authenticationSlice";
-// import GoogleAuth from "./GoogleAuth";
+import GoogleAuth from "./GoogleAuth";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -14,7 +14,7 @@ export default function AuthPage(props) {
     <>
     <div className="auth">
       <div className="auth-header">Аутентификация</div>
-      {/* <GoogleAuth from={from}/> */}
+      <GoogleAuth from={from}/>
       <button className="auth-sbis">
         <div className="auth-sbis__logo"></div>
         <div className="auth-sbis__text">Авторизоваться через СБИС</div>
