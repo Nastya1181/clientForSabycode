cdc76ee333cf2f5551bda3651c171d2b76dff26b
Jora947 <JoraKochkin@yandex.ru>
2022-07-27 19:25:18 +0500

    Очень важный коммит

--- src/components/Meeting.jsx
+++ src/components/Meeting.jsx
@@ -3,13 +3,36 @@ import logo from '../img/delete.svg';
 import { useRemoveConnectionMutation } from "../redux/api/sabycodeApi";
 export default function Meeting(props) {
   const [removeConnection, {}] = useRemoveConnectionMutation();
-    return (
-        <tbody id={props.id} className="container"><tr>
-        <td className="begin">{props.post.time}</td>
-            <td className="edit">{props.post.edited}</td>
-            <td className="named__lang font-weight">{props.post.prog}</td>
-            <td className="family">{props.post.name}</td>
-          <td><button className="delete__button"><div className="delete__inside"><img className="delete__logo" src={logo} onClick={async (event) => {console.log(event.target.parentNode.parentNode);await removeConnection({stat: event.target.parentNode.parentNode.id}).unwrap()}} /><span className="delete__meeting">Удалить</span></div></button></td></tr>
-        </tbody>
-    );
+  return (
+    <tbody>
+      <tr
+        id={props.id}
+        className="container"
+        onClick={(event) => {
+          navigate(`/editor/${event.currentTarget.id.split(".")[0]}`);
+        }}
+      >
+        <td className="begin">{props.time}</td>
+        <td className="edit">{props.edited}</td>
+        <td className="named__lang font-weight">{props.prog}</td>
+        <td className="family">{props.name}</td>
+        <td>
+          <button
+            className="delete__button"
+            id={props.id}
+            onClick={async (event) => {
+              console.log(event.currentTarget.id);
+              event.stopPropagation();
+              await removeConnection({ stat: event.currentTarget.id }).unwrap();
+            }}
+          >
+            <div className="delete__inside">
+              <img className="delete__logo" src={logo} />
+              <span className="delete__meeting">Удалить</span>
+            </div>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  );
 }
